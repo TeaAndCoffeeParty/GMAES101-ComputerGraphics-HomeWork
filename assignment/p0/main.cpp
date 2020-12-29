f78eae1740c9865194b39db078b05121a07ed87c
chentuo <chentuo@soonsolid.com>
2020-12-29 13:34:53 +0800

    p0 test

--- assignment/p0/main.cpp
+++ assignment/p0/main.cpp
@@ -3,9 +3,19 @@
 #include <eigen3/Eigen/Dense>
 #include <iostream>
 
-
+/*3.1作业描述
+ * 给定一个点 P =(2,1), 将该点绕原点先逆时针旋转 45◦ ,再平移 (1,2), 计算出
+ * 变换后点的坐标(要求用齐次坐标进行计算)。
+ */
 int main()
 {
-	Eigen::Vector2f p(2, 1);
+    Eigen::Vector2d p(2, 1);
 	std::cout << p << std::endl;
+
+    double rotationAngle = 45.0/180.0*M_PI;
+    double rotationX = p(0,0)*std::cos(-rotationAngle) - p(1,0) *std::sin(-rotationAngle);
+    double rotationY = p(1,0)*std::cos(-rotationAngle) + p(0,0) *std::sin(-rotationAngle);
+    std::cout<<rotationX<<" "<<rotationY<<std::endl;
+    Eigen::Vector2d p0(rotationX, rotationY);
+    std::cout<<p0<<std::endl;
 }
